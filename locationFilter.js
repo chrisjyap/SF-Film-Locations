2cdf3774253da5423e04bbd8867de1697423e666
cyap <chrisjyap@gmail.com>
2014-11-16 11:53:28 +0000

    AJAX command in. Filters set up. Google geocode locations?

--- locationFilter.js
+++ locationFilter.js
@@ -1,6 +1,12 @@
+var BASE_URL = "http://data.sfgov.org/resource/yitu-d5am.json?";
+
+
 $(document).ready(function(){
 	google.maps.event.addDomListener(window, 'load', initialize);
 
+	$("filterVal").keypress(function(event) {
+                if (event.which == 13)  onClickLookup();
+        });
 });
 
 function initialize(){
@@ -15,4 +21,39 @@ function initialize(){
 		//disableDoubleClickZoom: true
 	}
 	var map = new google.maps.Map(mapCanvas, mapOptions);
+	
+	var myLatLng = new google.maps.LatLng(37.75161126, -122.4463385); //"Justin Herman Plaza");
+        var marker = new google.maps.Marker({
+                position: myLatLng,
+    		map: map,
+		title: 'Testing!'
+        });
+}
+
+function onClickLookup(){
+	//TODO: add validation
+	var filter = document.querySelector('input[name="filter"]:checked').value;
+	var filterVal = (document.filterForm.filterVal.value).toLowerCase().replace(/ /g,'');
+	console.log( buildURL(BASE_URL, filter, filterVal) );
+	$.ajax( buildURL(BASE_URL, filter, filterVal) , {
+		type: 'GET',
+		//data: data,
+		dataType:"json",
+		success: function(data){
+			console.log(JSON.stringify(data));
+			if(data.length >0) getLocations(JSON.stringify(data));
+		},	
+		error: function(req, status, err){
+			console.log("Yeah, no. ", status, err);
+		}
+	});
+
+}
+
+function buildURL(baseURL, filter, filterVal){
+	return baseURL + filter + "=" + filterVal;
+}
+
+function getLocations(data){
+	console.log("Get locations: " + data);
 }
