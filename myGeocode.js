5f7eae9da9083f8ba9da1fee858c060e272c64de
cyap <chrisjyap@gmail.com>
2014-11-18 17:42:01 +0000

    Wasn't able to meet deadline. Multiple async requests came back randomly or gave setImmediate error messages. Will still continue to flush this out.

--- myGeocode.js
+++ myGeocode.js
@@ -17,15 +17,10 @@ app.get('/', function(req, res){
 		whoo: "Eunji"
 	};
 	var locations = req.query.locations;
-	var urls = [];
-	for(var i = 0; i< locations.length; i++){
-                locations[i] = locations[i].replace(/ /g,'+');
-		urls.push( buildURL(BASE_URL, locations[i],SECOND_URL, API_KEY) );
-	}
-	//console.log(urls);
+	somethingRequest(locations, res);
 	console.log("HTTP GET");
-	res.header('Access-Control-Allow-Origin', 'http://64.71.177.103');
-	res.send(test);
+	//res.header('Access-Control-Allow-Origin', 'http://64.71.177.103');
+	//res.send(test);
 });
 
 app.get('*', function(req, res, next) {
@@ -40,6 +35,44 @@ console.log('Express running on %s', server.address().port);
 
 
 function buildURL(base, address, city, key){
-	console.log("URLS: " + base+address+city+key);
+	//console.log("URLS: " + base+address+city+ key);
 	return base+ address + city+ key;
+}
+
+var __request = function (urls, callback) {
+ 
+	'use strict';
+ 
+	var results = {}, t = urls.length, c = 0,
+		handler = function (error, response, body) {
+ 			body = JSON.parse(body);
+			console.log(c + ": " + JSON.stringify(body.results)); 
+			results[body.results["formatted_address"]] = body; //{ error: error, response: response, body: body };
+			if (++c === urls.length) { callback(results); }
+		};
+
+	while (t--) { request(urls[t], handler); }
+};
+
+function somethingRequest(locations, res){
+	var urls = [];
+        var responses=[];
+        var completed_request = 0;
+	console.log("Break yet?");
+        for(var i = 0; i< locations.length; i++){
+                locations[i] = locations[i].replace(/ /g,'+');
+                urls.push( buildURL(BASE_URL, locations[i],SECOND_URL, API_KEY) );
+                http.get(url, function(response) {
+                    //responses.push(response.body);
+	           console.log("EHORALWJRKLAWJAWKL:RKLJ:AWJKLTJL:AKWT");
+                   completed_request++;
+                    if (completed_request == locations.length) {
+                          // All download done, process responses array
+                         console.log(responses);
+			//res.header('Access-Control-Allow-Origin', 'http://64.71.177.103');
+                	//res.send(responses);	 
+                    }
+                });
+        }
+
 }
